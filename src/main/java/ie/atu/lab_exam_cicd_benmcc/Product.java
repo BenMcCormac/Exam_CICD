c3616d948c0e90555a2e33d601c15ce55218a09d
G00416547@atu.ie <G00416547@atu.ie>
2024-11-05 11:34:25 +0000

    Added Put and Delete functions

--- src/main/java/ie/atu/lab_exam_cicd_benmcc/Product.java
+++ src/main/java/ie/atu/lab_exam_cicd_benmcc/Product.java
@@ -14,13 +14,17 @@ public class Product
 {
     @NotBlank(message ="Code can't be blank")
     private String pCode;
+
     @NotBlank(message ="Name can't be blank")
     private String pName;
+
     @NotBlank(message ="Category can't be blank")
     private String category;
+
     @NotNull(message ="Amount cant be null")
     @PositiveOrZero(message ="Price cant be less than 0")
     private int amount;
+
     @NotNull(message ="Price cant be null")
     @PositiveOrZero(message ="Price cant be less than 0")
     private int price;
